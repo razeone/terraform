86a1f767b3d536ea276a706ba75f19b32d78aaf9
Jorge Raze <jorge.raze@clevertech.biz>
2020-02-07 19:36:40 -0600

    Move AWS repo

---
+++ s3/main.tf
@@ -0,0 +1,46 @@
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website_test" {
+  bucket = "s3-website-test.raze.mx"
+  acl    = "public-read"
+  # policy = "${file("policy.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "${aws_s3_bucket.website_test.id}"
+
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+	"Sid":"PublicReadGetObject",
+        "Effect":"Allow",
+	  "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::s3-website-test.raze.mx/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
