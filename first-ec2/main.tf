69b73a226501dcdda500bd63ec4ca6aca7c85f12
Jorge Raze <jorge.raze@clevertech.biz>
2020-02-07 23:50:34 -0600

    Update EC2

--- first-ec2/main.tf
+++ first-ec2/main.tf
@@ -3,8 +3,22 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+locals {
+  common_tags = {
+    Component   = "awesome-app"
+    Environment = "production"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  tags          = map("Name", "MyEC2")
+  
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "awesome-app-server",
+      "Role", "server"
+    )
+  )}"
 }
