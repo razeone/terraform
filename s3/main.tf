ef994082f0d43cca4023cb0e126da2d9771921d8
Jorge Raze <jorge.raze@clevertech.biz>
2020-02-08 19:11:32 -0600

    Refactor s3

--- s3/main.tf
+++ s3/main.tf
@@ -4,10 +4,20 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "bucket_name" {
+  description = "Bucket name"
+  default     = "s3-website-test"
+}
+
+variable "domain_name" {
+  description = "Parent domain name"
+  default     = "raze.mx"
+}
+
+
 resource "aws_s3_bucket" "website_test" {
-  bucket = "s3-website-test.raze.mx"
+  bucket = "${var.bucket_name}.${var.domain_name}"
   acl    = "public-read"
-  # policy = "${file("policy.json")}"
 
   website {
     index_document = "index.html"
@@ -26,23 +36,22 @@ EOF
   }
 }
 
+data "aws_iam_policy_document" "bucket_policy_doc" {
+  statement {
+    sid       = "PublicReadGetObject"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.bucket_name}.${var.domain_name}/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = "${aws_s3_bucket.website_test.id}"
-
-  policy = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[{
-	"Sid":"PublicReadGetObject",
-        "Effect":"Allow",
-	  "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::s3-website-test.raze.mx/*"
-      ]
-    }
-  ]
-}
-POLICY
+  policy = "${data.aws_iam_policy_document.bucket_policy_doc.json}"
 }
 
 output "s3_website_url" {
