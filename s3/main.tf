82417b2c62bfccb8bcbc65ad58aa8379cbebc65e
Jorge Raze <jorge.raze@clevertech.biz>
2020-02-08 19:18:21 -0600

    Format

--- s3/main.tf
+++ s3/main.tf
@@ -37,7 +37,6 @@ data "aws_iam_policy_document" "bucket_policy_doc" {
   }
 }
 
-
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = "${aws_s3_bucket.website_test.id}"
   policy = "${data.aws_iam_policy_document.bucket_policy_doc.json}"
