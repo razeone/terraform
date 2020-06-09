d641e3f5d9639c7a332af69e7f9ef315d5cbe834
Jorge Alcaraz <jorge.alcaraz@rackspace.com>
2020-06-09 13:06:07 -0500

    Add some extra examples

---
+++ tf-instance/example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example1" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
